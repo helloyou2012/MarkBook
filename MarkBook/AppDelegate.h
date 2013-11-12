8e3c1cc38b24b0233680e863da1e8ba8f785ff0c
helloyou2012 <helloyou2012@yeah.net>
2013-11-12 19:42:03 +0800

    初始化

---
+++ MarkBook/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MarkBook
+//
+//  Created by ZhenzhenXu on 11/9/13.
+//  Copyright (c) 2013 ZhenzhenXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@end
